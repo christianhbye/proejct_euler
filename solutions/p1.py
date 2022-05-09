ed1f26027fac7b8c6ccccf9441838b5affbdbe68
Christian Hellum Bye <chbye@berkeley.edu>
2022-05-08 20:05:28 -0700

    blacken code

--- solutions/p1.py
+++ solutions/p1.py
@@ -1,14 +1,15 @@
-
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these 
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these
 # multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
 
 import numpy as np
 
+
 def sum_of_multiple(n):
-    s_arr=n*np.arange(1000//n+1)
-    s_arr=s_arr[s_arr<1000]
-    s=np.sum(s_arr)
+    s_arr = n * np.arange(1000 // n + 1)
+    s_arr = s_arr[s_arr < 1000]
+    s = np.sum(s_arr)
     return s
 
-ans=sum_of_multiple(3)+sum_of_multiple(5)-sum_of_multiple(3*5)
+
+ans = sum_of_multiple(3) + sum_of_multiple(5) - sum_of_multiple(3 * 5)
 print(ans)
--- solutions/p2.py
+++ solutions/p2.py
@@ -4,12 +4,12 @@
 import numpy as np
 
 # brute force code
-fib=[1, 2]
-ans=2
-while fib[-1]<4e6:
-    next_val=fib[-2]+fib[-1]
-    if next_val%2==0:
-        ans+=next_val
+fib = [1, 2]
+ans = 2
+while fib[-1] < 4e6:
+    next_val = fib[-2] + fib[-1]
+    if next_val % 2 == 0:
+        ans += next_val
     fib.append(next_val)
 print(ans)
 
