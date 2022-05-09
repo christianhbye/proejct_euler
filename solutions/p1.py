c54a714d024b0ff17b3fc231fdd3a1d2f49ff8a8
Christian Hellum Bye <chbye@berkeley.edu>
2022-05-08 20:19:34 -0700

    simplify

--- solutions/p1.py
+++ solutions/p1.py
@@ -1,15 +1,8 @@
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these
-# multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the
+multiples of 3 or 5 below 1000.
+"""
 
-import numpy as np
-
-
-def sum_of_multiple(n):
-    s_arr = n * np.arange(1000 // n + 1)
-    s_arr = s_arr[s_arr < 1000]
-    s = np.sum(s_arr)
-    return s
-
-
-ans = sum_of_multiple(3) + sum_of_multiple(5) - sum_of_multiple(3 * 5)
+ans = sum([x for x in range(1000) if x % 3 == 0 or x % 5 == 0])
 print(ans)
